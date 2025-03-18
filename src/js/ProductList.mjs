ffd63bacb5e8b8b897d8629552f548f19df643d2
Ross L. Owen <rowen@byupathway.edu>
2025-03-18 14:16:21 -0600

    week3: show discounted price if discounted

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,6 +1,9 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
+  const showDiscount = product.SuggestedRetailPrice > product.FinalPrice
+    ? ""
+    : "hidden";
     return `<li class="product-card">
             <a href="product_pages/?product=${product.Id}">
               <img
@@ -9,7 +12,8 @@ function productCardTemplate(product) {
               />
               <h3 class="card__brand">${product.Brand.Name}</h3>
               <h2 class="card__name">${product.Name}</h2>
-              <p class="product-card__price"$${product.FinalPrice}</p></a
+              <p class="product-card__retail ${showDiscount}">$${product.SuggestedRetailPrice}</p>
+              <p class="product-card__price">$${product.FinalPrice}</p></a
             >
           </li>`
 }
