7fda7bbce6894ec25e0625fd6c5b31e468fa30c4
Ben Johansen <johansenben12@gmail.com>
2025-03-25 16:50:47 -0600

    week04 team activity

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,2 +1,14 @@
+import CheckoutProcess from "./CheckoutProcess.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
+
+const checkoutProcess = new CheckoutProcess(document.querySelector(".order-summary"));
+checkoutProcess.init();
+
+document.querySelector("#checkout").addEventListener("submit", e => {
+    e.preventDefault();
+    checkoutProcess.checkout(e.target);
+});
+
+
+
 await loadHeaderFooter();
